425094808efaccfae6b7da4cba1e82a7f6bc5ef7
artyom2006KBTU <1000th10@gmail.com>
2024-10-17 18:09:23 +0500

    Create ex11.cpp
    Входные данные
    Даны два целых числа, каждое число записано в отдельной строке.
    
    Выходные данные
    Выведите наибольшее из данных чисел.
    
    Примеры
    Входные данные
    1
    2
    Выходные данные
    2

---
+++ ex11.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int n1;
+    cin>>n1;
+    if(n>n1){
+        cout<<n;
+    }
+    else if(n1>n){
+        cout<<n1;
+    }
+    
+    return 0;
+}
