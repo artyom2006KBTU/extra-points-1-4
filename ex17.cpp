7db39cc467b555f55f60a76c579b42712c7181d7
artyom2006KBTU <1000th10@gmail.com>
2024-10-17 20:32:53 +0500

    Create ex17.cpp
    Требуется определить, бьет ли ладья, стоящая на клетке с указанными координатами (номер строки и номер столбца), фигуру, стоящую на другой указанной клетке.
    
    Входные данные
    Вводятся четыре числа: координаты ладьи (два числа) и координаты другой фигуры (два числа), каждое число вводится в отдельной строке. Координаты - целые числа в интервале от 1 до 8.
    
    Выходные данные
    Требуется вывести слово YES, если ладья сможет побить фигуру за 1 ход и NO - в противном случае.
    
    Примеры
    Входные данные
    1
    1
    2
    2
    Выходные данные
    NO
    Входные данные
    1
    1
    2
    1
    Выходные данные
    YES

---
+++ ex17.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c,d;
+    cin >> a >> b >> c>>d;
+    if(a==c){
+        cout<<"YES";
+    }
+    else if(b==d){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+
+
+    return 0;
+}
