78563830934348aaa6d4928377e2e60f90cb4284
artyom2006KBTU <1000th10@gmail.com>
2024-10-17 18:19:40 +0500

    Create ex15.cpp
    Входные данные
    Даны два целых числа, каждое записано в отдельной строке.
    
    Выходные данные
    Программа должна вывести число 1, если первое число больше второго, число 2, если второе больше первого, или число 0, если они равны.
    
    Примеры
    Входные данные
    1
    2
    Выходные данные
    2

---
+++ ex15.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int n1;
+    cin>>n1;
+    if(n>n1){
+        cout<<1;
+    }
+    else if(n1>n){
+        cout<<2;
+    }
+    
+    return 0;
+}
+
