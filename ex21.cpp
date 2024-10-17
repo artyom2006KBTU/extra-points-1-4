160502fe47324be1dae3f4e09830f2de9ed35bb7
artyom2006KBTU <1000th10@gmail.com>
2024-10-17 20:40:44 +0500

    Create ex21.cpp
    Входные данные
    Вводятся целые числа a и b. Гарантируется, что a не превосходит b
    
    Выходные данные
    Выведите (через пробел) все четные числа от a до b (включительно).
    
    Примеры
    Входные данные
    2
    5
    Выходные данные
    2 4

---
+++ ex21.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int n1;
+    cin>>n1;
+    for(int i=n;i<=n1;i++){
+        if(i%2==0){
+            cout<<i<<endl;
+        }
+    }
+
+
+
+
+
+
+}
